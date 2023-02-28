74e9e87dfdf9b8a05c89d85155c1c779ea6853ff
Diego Lima <xxdiegolsxx@gmail.com>
2023-02-28 13:49:13 -0300

    learning

--- src/components/Greeting.test.tsx
+++ src/components/Greeting.test.tsx
@@ -3,9 +3,15 @@ import { describe } from 'vitest';
 import { Greeting } from './Greeting';
 
 describe('Greeting tests', () => {
-  it('should render Greeting without name', () => {
+  it('should render Greeting with a name', () => {
     render(<Greeting name='Diego'/>);
     const text = screen.getByText(/diego/i)
     expect(text).toBeInTheDocument();
   });
+
+  it('should render Greeting without a name', ()=> {
+    render(<Greeting />);
+    const text = screen.getByText(/guest/i);
+    expect(text).toBeInTheDocument();
+  })
 });
